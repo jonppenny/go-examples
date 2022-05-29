f47737799f18a77f66fcab5439e2a93703819d68
Jonathan Penny <jonathan.penny@gmail.com>
2022-05-29 19:35:35 +0100

    Some reformatting

--- exercises/1.quiz/main.go
+++ exercises/1.quiz/main.go
@@ -1,27 +1,20 @@
 package main
 
 import (
-	"encoding/csv"
 	"fmt"
 	"log"
-	"os"
 )
-
-type csvRecords struct {
-	question string
-	answer   string
-}
 
 func main() {
 	var score int
 
-	records, err := readFile("problems.csv")
+	records, err := ReadFile("problems.csv")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
 	for i, record := range records {
-		data := csvRecords{
+		data := Record{
 			question: record[0],
 			answer:   record[1],
 		}
@@ -34,21 +27,4 @@ func main() {
 	}
 
 	fmt.Printf("You scored: %d\n", score)
-}
-
-// Read a file
-func readFile(file string) ([][]string, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return [][]string{}, err
-	}
-	defer f.Close()
-
-	cr := csv.NewReader(f)
-	records, err := cr.ReadAll()
-	if err != nil {
-		return [][]string{}, err
-	}
-
-	return records, nil
 }
---
+++ exercises/1.quiz/record.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/csv"
+	"os"
+)
+
+type Record struct {
+	question string
+	answer   string
+}
+
+// ReadFile Read a file. Must be a CSV.
+// TODO check file type and return error if not CSV.
+func ReadFile(file string) ([][]string, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return [][]string{}, err
+	}
+	defer f.Close()
+
+	cr := csv.NewReader(f)
+	records, err := cr.ReadAll()
+	if err != nil {
+		return [][]string{}, err
+	}
+
+	return records, nil
+}
