68c63e45b5e80e3e16fc68b42f6fc86524fa5033
Jonathan Penny <jonathan.penny@gmail.com>
2023-07-02 18:05:33 +0100

    Updates to fizzbuzz

--- fizzbuzz/main.go
+++ fizzbuzz/main.go
@@ -3,25 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fizzBuzz(30)
+	fmt.Println("Enter a whole number")
+	var num int
+	fmt.Scanln(&num)
+
+	for i := 0; i <= num; i++ {
+		fizzBuzz(i)
+	}
 }
 
 // Solution for the FizzBuzz problem
-func fizzBuzz(n int) {
-	for i := 1; i < n; i++ {
-		switch {
-		case i%3 == 0 && i%5 == 0:
-			fmt.Printf("fizz buzz\n")
-			break
-		case i%3 == 0:
-			fmt.Printf("fizz\n")
-			break
-		case i%5 == 0:
-			fmt.Printf("buzz\n")
-			break
-		default:
-			fmt.Printf("%d\n", i)
-			break
-		}
+func fizzBuzz(i int) {
+	switch {
+	case i%3 == 0 && i%5 == 0:
+		fmt.Printf("fizz buzz\n")
+		break
+	case i%3 == 0:
+		fmt.Printf("fizz\n")
+		break
+	case i%5 == 0:
+		fmt.Printf("buzz\n")
+		break
+	default:
+		fmt.Printf("%d\n", i)
+		break
 	}
 }
